4c493ec4466af008944f6630b23602c950b70210
FireHawkss <mert.vergote1@gmail.com>
2024-02-06 00:49:16 +0100

    added functionality to conevert gtfs zip to SQLite database

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 from gtfs_functions import Feed
+import csv
+import pandas
+from sqlalchemy import create_engine
+
+def create_sql_database(name,list_of_files):
+
+    # Specify the SQLite database file path with .db extension
+    db_path = name+"_static_data.db"
+    # Create a SQLite engine
+    engine = create_engine(f'sqlite:///{db_path}')
+
+    for file in list_of_files:
+        csv_file = pandas.read_csv(name + "_unpacked/" + file + ".txt")
+        # Write the DataFrame to the SQLite database
+        csv_file.to_sql(file, engine, index=False, if_exists='replace')
+
+
+izmir_list_of_files = ['agency','calendar','calendar_dates','routes','stop_times','stops','trips']
+NMBS_list_of_files = ['agency','calendar','calendar_dates','routes','stop_times','stop_time_overrides','stops','transfers','translations','trips']
+
+create_sql_database("izmir_tram", izmir_list_of_files)
+create_sql_database("NMBS", NMBS_list_of_files)
+
+
+#print(test)
 
 
 gtfs_path = 'rail-tramizmir-gtfs.zip'
 
-feed = Feed(gtfs_path, time_windows=[0, 6, 10, 12, 16, 19, 24])
+#feed = Feed(gtfs_path, time_windows=[0, 6, 10, 12, 16, 19, 24])
 
 
-for entity in feed.entity:
-  if entity.HasField('trip_update'):
-    print(entity.trip_update)
-    print("     SHITE       ")
+
 
 #parsed_calendar = Feed(gtfs_path).parse_calendar()
 
--- main_realtime.py
+++ main_realtime.py
@@ -4,5 +4,9 @@ import requests
 feed = gtfs_realtime_pb2.FeedMessage()
 response = requests.get('https://sncb-opendata.hafas.de/gtfs/realtime/c21ac6758dd25af84cca5b707f3cb3de')
 feed.ParseFromString(response.content)
+for entity in feed.entity:
+  if entity.HasField('trip_update'):
+    print(entity.trip_update)
+    print("     SHITE       ")
 
 print(feed.entity)
